5851728a783cebe530d1be09c080ec4d5dfa5734
armishra111 <168307101+armishra111@users.noreply.github.com>
2024-12-23 23:16:50 +0000

    Delete Bose_Hubbard directory

--- Bose_Hubbard/Creation_Operator.m
+++
@@ -1,6 +0,0 @@
-function result = Creation_Operator(index,vect)
-    vect.v(index) = vect.v(index) + 1;
-    vect.c = vect.c*sqrt(vect.v(index));
-    result = vect;
-end
-
--- Bose_Hubbard/Number_Operator.m
+++
@@ -1,5 +0,0 @@
-function result = Number_Operator(index, vect)
-    vect.c = vect.c*vect.v(index);
-    result = vect;
-end
-
